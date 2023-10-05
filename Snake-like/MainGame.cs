673e8fe56cbdd7c78439d550275e515e25c07ad4
Daniel Hoppe Hansen <dhha58385@edu.ucl.dk>
2023-10-05 22:45:29 +0200

    Improved functionality of "MakeBorder" method. Added "TestMovement" method allowing you to move a box around in console application (not finished).

--- Snake-like/MainGame.cs
+++ Snake-like/MainGame.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
+            //Console.BufferHeight = 300;
+            //Console.BufferWidth = 300;
+            //Console.WindowHeight = 200;
+            //Console.WindowWidth = 200;
+
             Snake snake = new Snake();
-            snake.MakeBorder();
+            //snake.MakeBorder();
+            snake.MakeBorder(true);
+            snake.TestMovement();
 
             Console.ReadKey();
         }
--- Snake-like/Snake.cs
+++ Snake-like/Snake.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,7 @@ namespace Snake_like
     public class Snake
     {
         public int XPos { get; set; }
-        public int Ypos { get; set; }
+        public int YPos { get; set; }
         public int Length { get; set; }
         public int Screenwidth { get; set; } = Console.WindowWidth;
         public int Screenheight { get; set; } = Console.WindowHeight;
@@ -17,33 +18,97 @@ namespace Snake_like
         public Snake()                // Constructor for the snake. 
         {
             XPos = Screenwidth / 2;
-            Ypos = Screenheight / 2;
+            YPos = Screenheight / 2;
             Length = 3; 
         }
 
-        public void MakeBorder()     // Makes the border of the map in a fun way. (5 ms interval)
+        public void MakeBorder(bool DoInstant)      // Makes the border of the map in a fun way. (5 ms interval)
         {
+            Console.CursorVisible = false;
+            if (!DoInstant)
+            {
+                string loading = "Game is loading...";
+                Console.SetCursorPosition(Screenwidth / 2 - loading.Length, Screenheight / 2);
+                Console.Write(loading);
+            }
             for (int i = 0; i < Screenwidth; i++)
             {
                 Console.SetCursorPosition(i, 0);
                 Console.Write("■");
-                Thread.Sleep(5);
+                if (!DoInstant)
+                    Thread.Sleep(5);
             }
             for (int i = 0; i < Screenheight; i++)
             {
                 Console.SetCursorPosition(Screenwidth - 1, i);
                 Console.Write("■");
-                Thread.Sleep(5);
+                if (!DoInstant)
+                    Thread.Sleep(5);
             }
             for (int i = Screenwidth-1; i > 0; i--)
             {
                 Console.SetCursorPosition(i, Screenheight - 1);
                 Console.Write("■");
-                Thread.Sleep(5);
+                if (!DoInstant)
+                    Thread.Sleep(5);
             }
             for (int i = Screenheight-1; i > 0; i--)
             {
                 Console.SetCursorPosition(0, i);
+                Console.Write("■");
+                if (!DoInstant)
+                    Thread.Sleep(5);
+            }
+        }
+
+        public void TestMovement()
+        {
+            //          #### Initialize ####
+
+            Console.CursorVisible = false;
+            Random random = new Random();
+            XPos = random.Next(1, Screenwidth);
+            YPos = random.Next(1, Screenheight);
+
+            //          #### Skrt ####
+
+            Console.SetCursorPosition(XPos,YPos);
+            Console.Write("■");
+
+            //            The main movement loop
+            while(true)
+            {
+                ConsoleKeyInfo arrow = Console.ReadKey(true);
+                switch (arrow.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        if (YPos < Screenheight - 1)
+                            YPos += 1;
+                        //Console.Clear();
+                        //MakeBorder(true);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (YPos > 1)
+                            YPos -= 1;
+                        //Console.Clear();
+                        //MakeBorder(true);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (XPos > 1)
+                            XPos -= 2;
+                        //Console.Clear();
+                        //MakeBorder(true);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (XPos < Screenwidth-1)
+                            XPos += 2;
+                        //Console.Clear();
+                        //MakeBorder(true);
+                        break;
+                    default:
+                        break;
+                }
+                Console.SetCursorPosition(XPos, YPos);
                 Console.Write("■");
                 Thread.Sleep(5);
             }
