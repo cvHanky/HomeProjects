9090fce27bd40a7259eb659c5beabf225966b938
cvHanky <145098299+cvHanky@users.noreply.github.com>
2024-07-04 13:17:16 +0200

    Created a black rectangle, animated by 2 buttons. Created a picture of a robot, animated by hovering over it.

---
+++ AnimationsInWPF/AnimationsInWPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimationsInWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartAnimation_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = (Storyboard)this.Resources["rctAnimation"];
+            sb.Begin();
+        }
+
+        private void btnHoverAnimation_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = (Storyboard)this.Resources["HoverAnimation"];
+            sb.Begin();
+        }
+
+        private void imgRobot_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Storyboard sb = (Storyboard)this.Resources["RobotHover"];
+            sb.Begin();
+        }
+
+        private void imgRobot_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Storyboard sb = (Storyboard)this.Resources["RobotHover"];
+            sb.Stop();
+        }
+    }
+}
